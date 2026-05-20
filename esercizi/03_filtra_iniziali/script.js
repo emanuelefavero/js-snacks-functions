f3bcd2c073344d7a368ed48f8570d60840836706
Emanuele Favero <emafaux89@gmail.com>
2026-05-20 19:39:31 +0200

    refactor: solution 03 to better follow exercise requirements

--- esercizi/03_filtra_iniziali/script.js
+++ esercizi/03_filtra_iniziali/script.js
@@ -4,19 +4,30 @@ const names = ['Anna', 'Luca', 'Marco', 'Adele', 'Laura', 'Alessandra'];
 
 // Arrow
 {
-  const filterByInitial = (names, char) =>
-    names.filter(name => name.startsWith(char));
+  const filterByInitial = (names, letter) => {
+    const result = [];
+    const lowerCaseLetter = letter.charAt(0).toLowerCase();
+
+    for (const name of names) {
+      const firstLetter = name.charAt(0).toLowerCase();
+      if (firstLetter === lowerCaseLetter) result.push(name);
+    }
+
+    return result;
+  };
 
   console.log(filterByInitial(names, 'A'));
 }
 
 // Classic
 {
-  function filterByInitial(names, char) {
+  function filterByInitial(names, letter) {
     const result = [];
+    const lowerCaseLetter = letter.charAt(0).toLowerCase();
 
     for (const name of names) {
-      if (name[0] === char) result.push(name);
+      const firstLetter = name.charAt(0).toLowerCase();
+      if (firstLetter === lowerCaseLetter) result.push(name);
     }
 
     return result;
